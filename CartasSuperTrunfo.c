f867142423137f9e07ef5a0ce17a83f80fd80920
WILSON SOARES JUNIOR <willipixuna@gmail.com>
2025-08-19 14:50:57 +0000

    commit alteracao

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,7 @@ int main(){
     char estado1[50];
     char codigo1[50];
     char nome1[50];
-    unsigned long int populacao1;
+    float populacao1;
     float area1;
     float pib1;
     int ponto1;
@@ -23,7 +23,7 @@ int main(){
     scanf("%s", nome1);
 
     printf("População1: ");
-    scanf("%lu", &populacao1);
+    scanf("%f", &populacao1);
 
     printf("Área: ");
     scanf("%f", &area1);
@@ -34,76 +34,75 @@ int main(){
     printf("Numero de Pontos Turisticos: ");
     scanf("%d", &ponto1);
 
-    printf("Decidade Populacional: ");
-    scanf("%f", &dencidade1);
+    dencidade1 = populacao1 / area1;
+    per_capita1 = pib1 / populacao1;
 
-    printf("PIB per Capita: ");
-    scanf("%d", &per_capita1);
+
+    printf("*** Dados da Carta 1**\n");
     
-
     printf("Estado1: %s\n", estado1);
     printf("Código1: %s\n", codigo1);
     printf("Nome da Cidade: %s\n", nome1);
-    printf("População1: %lu habitantes\n", populacao1);
-    printf("Área: %f km²\n", area1);
-    printf("PIB: %f bilhões\n", pib1);
+    printf("População1: %.0f habitantes\n", populacao1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões\n", pib1);
     printf("Numero de Pontos Turisticos: %d\n", ponto1);
-    printf("Decidade Populacional: %d\n", dencidade1);
-    printf("PIB per Capita: %d\n", per_capita1);
+    printf("Decidade Populacional: %.2f hab/km²\n", dencidade1);
+    printf("PIB per Capita: %.2f reais\n", per_capita1);
 
 
 
 
 
 
-    printf("***Carta 2***\n");
-    char estado2[50];
-    char codigo2[50];
-    char nome2[50];
-    unsigned long int populacao2;
-    float area2;
-    float pib2;
-    int ponto2;
-    float dencidade2;
-    float per_capita2;
+    //printf("***Carta 2***\n");
+    //char estado2[50];
+    //char codigo2[50];
+    //char nome2[50];
+    //unsigned long int populacao2;
+    //float area2;
+    //float pib2;
+    //int ponto2;
+    //float dencidade2;
+    //float per_capita2;
 
-    printf("Estado2: ");
-    scanf("%s", estado2);
+    //printf("Estado2: ");
+    //scanf("%s", estado2);
 
-    printf("Código2: ");
-    scanf("%s", codigo2);
+    //printf("Código2: ");
+    //scanf("%s", codigo2);
 
-    printf("Nome da Cidade: ");
-    scanf("%s", nome2);
+    //printf("Nome da Cidade: ");
+    //scanf("%s", nome2);
 
-    printf("População: ");
-    scanf("%lu", &populacao2);
+    //printf("População: ");
+    //scanf("%lu", &populacao2);
 
-    printf("Área: ");
-    scanf("%f", &area2);
+   // printf("Área: ");
+   // scanf("%f", &area2);
 
-    printf("PIB: ");
-    scanf("%f", &pib2);
+   // printf("PIB: ");
+   // scanf("%f", &pib2);
 
-    printf("Numero de Pontos Turisticos: ");
-    scanf("%d", &ponto2);
+   // printf("Numero de Pontos Turisticos: ");
+    //scanf("%d", &ponto2);
 
-    printf("Decidade Populacional: ");
-    scanf("%f", &dencidade2);
+   // printf("Decidade Populacional: ");
+   // scanf("%f", &dencidade2);
 
-    printf("PIB per Capita: ");
-    scanf("%d", &per_capita2);
+   // printf("PIB per Capita: ");
+   // scanf("%d", &per_capita2);
     
 
-    printf("Estado2: %s\n", estado2);
-    printf("Código2: %s\n", codigo2);
-    printf("Nome da Cidade: %s\n", nome2);
-    printf("População: %lu habitantes\n", populacao2);
-    printf("Área: %f km²\n", area2);
-    printf("PIB: %f bilhões\n", pib2);
-    printf("Numero de Pontos Turisticos: %d", ponto2);
-    printf("Decidade Populacional: %d\n", dencidade2);
-    printf("PIB per Capita: %d\n", per_capita2);
+   // printf("Estado2: %s\n", estado2);
+   // printf("Código2: %s\n", codigo2);
+   // printf("Nome da Cidade: %s\n", nome2);
+   // printf("População: %lu habitantes\n", populacao2);
+   // printf("Área: %f km²\n", area2);
+   // printf("PIB: %f bilhões\n", pib2);
+   // printf("Numero de Pontos Turisticos: %d", ponto2);
+   // printf("Decidade Populacional: %d\n", dencidade2);
+   // printf("PIB per Capita: %d\n", per_capita2);
 
    
 
