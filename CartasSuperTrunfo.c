74a344ac860cfbebf10329dac98a3520edf52de3
WILSON SOARES JUNIOR <willipixuna@gmail.com>
2025-08-21 17:52:37 +0000

    commit nivel mestre1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,12 +6,13 @@ int main(){
     char estado1[50];
     char codigo1[50];
     char nome1[50];
-    float populacao1;
+    unsigned long int populacao1;
     float area1;
     float pib1;
     int ponto1;
     float dencidade1;
     float per_capita1;
+
 
     printf("Estado1: ");
     scanf("%s", estado1);
@@ -23,7 +24,7 @@ int main(){
     scanf("%s", nome1);
 
     printf("População1: ");
-    scanf("%f", &populacao1);
+    scanf("%lu", &populacao1);
 
     printf("Área: ");
     scanf("%f", &area1);
@@ -44,7 +45,7 @@ int main(){
     printf("Estado1: %s\n", estado1);
     printf("Código1: %s\n", codigo1);
     printf("Nome da Cidade: %s\n", nome1);
-    printf("População1: %.0f habitantes\n", populacao1);
+    printf("População1: %lu habitantes\n", populacao1);
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões\n", pib1);
     printf("Numero de Pontos Turisticos: %d\n", ponto1);
@@ -60,7 +61,7 @@ int main(){
     char estado2[50];
     char codigo2[50];
     char nome2[50];
-    float populacao2;
+    unsigned long int populacao2;
     float area2;
     float pib2;
     int ponto2;
@@ -77,7 +78,7 @@ int main(){
     scanf("%s", nome2);
 
     printf("População: ");
-    scanf("%f", &populacao2);
+    scanf("%lu", &populacao2);
 
     printf("Área: ");
     scanf("%f", &area2);
@@ -96,14 +97,23 @@ int main(){
     printf("Estado2: %s\n", estado2);
     printf("Código2: %s\n", codigo2);
     printf("Nome da Cidade: %s\n", nome2);
-    printf("População: %.0f habitantes\n", populacao2);
-    printf("Área: %.2f km²\n", area2);
-    printf("PIB: %.2f bilhões\n", pib2);
-    printf("Numero de Pontos Turisticos: %d\n", ponto2);
+    printf("População2: %lu habitantes\n", populacao2);
+    printf("Área2: %.2f km²\n", area2);
+    printf("PIB2: %.2f bilhões\n", pib2);
+    printf("Numero de Pontos Turisticos2: %d\n", ponto2);
     printf("Decidade Populacional: %.2f hab/km²\n", dencidade2);
     printf("PIB per Capita: %.2f reais\n", per_capita2);
 
    
+
+
+
+
+
+
+
+
+
 
     return 0;
 
